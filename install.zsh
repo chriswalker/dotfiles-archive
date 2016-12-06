5a91bd9752232c08f3dcd7edc65ccefd8d1e0a87
Chris Walker <thatamazingweb@gmail.com>
2016-12-06 13:51:10 +0000

    Add fish links; change directory paths

--- install.zsh
+++ install.zsh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-cd ~
-ln -fs dotfiles/.zsh/.zshrc .
-ln -fs dotfiles/.tmux.conf .
-ln -fs dotfiles/.ackrc .
-ln -fs dotfiles/.vim/.vimrc .
+#ln -fs dotfiles/.zsh/.zshrc .
+ln -fs ~/dotfiles/.tmux.conf ~
+ln -fs ~/dotfiles/.ackrc ~
+ln -fs ~/dotfiles/.vim/.vimrc ~
+#ln -fs ~/dotfiles/.bashrc ~
+
+rm -rf ~/.config/fish
+ln -fs ~/dotfiles/fish ~/.config/fish
