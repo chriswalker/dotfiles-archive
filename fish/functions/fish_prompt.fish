985b58c9ef025b0908ecbfde46ea3388679c031e
Chris Walker <thatamazingweb@gmail.com>
2017-01-04 10:21:35 +0000

    Modify prompt style

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -7,16 +7,15 @@ function fish_prompt --description 'Write out the prompt with colours based on b
     set -g fish_prompt_pwd_dir_length 0
 
     # set up some colour variables
-    set -g blue (set_color blue)
-    set -g yellow (set_color yellow)
-    set -g bold_orange (set_color --background black --bold red)
+    set -g blue (set_color --bold --background black blue)
+    set -g yellow (set_color --bold yellow) 
+    set -g bold_orange (set_color --background black --bold brred)
 
     set -g time (date "+%H:%M:%S")
 
     printf $blue
     printf '[%s]' $time
-    printf ' '
-    printf '%s@%s ' $USER $__fish_prompt_hostname
+    printf ' %s@%s ' $USER $__fish_prompt_hostname
     printf $yellow
     printf '%s\n' (prompt_pwd) 
     printf $bold_orange
