895dace9f737aa00a5d26cebcff820d6400bb99f
Chris Walker <thatamazingweb@gmail.com>
2017-02-25 21:41:12 +0000

    Remove background colours from prompt

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -7,9 +7,9 @@ function fish_prompt --description 'Write out the prompt with colours based on b
     set -g fish_prompt_pwd_dir_length 0
 
     # set up some colour variables
-    set -g blue (set_color --bold --background black blue)
+    set -g blue (set_color --bold blue)
     set -g yellow (set_color --bold yellow) 
-    set -g bold_orange (set_color --background black --bold brred)
+    set -g bold_orange (set_color --bold brred)
 
     set -g time (date "+%H:%M:%S")
 
