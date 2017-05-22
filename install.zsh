587498b156b43969cefdf57523e4dc1526984757
Chris Walker <thatamazingweb@gmail.com>
2017-05-22 21:10:09 +0100

    Link screen-256color terminfo if OS = Darwin

--- install.zsh
+++ install.zsh
@@ -9,4 +9,11 @@ ln -fs ~/dotfiles/.vim/.vimrc ~
 rm -rf ~/.config/fish ~/.config/nvim
 
 ln -fs ~/dotfiles/fish ~/.config/fish
-ln -fs ~/dotfile/nvim ~/.config/nvim
+ln -fs ~/dotfiles/nvim ~/.config/nvim
+
+# Tweaked terminfo for tmux's $TERM, replacing ^H (backspace)
+# with ASCII DEL. Allows Ctrl-h to work when remapped in (N)Vim/tmux
+# for moving to the next-left window
+if [[ `uname` == 'Darwin' ]]; then
+  ln -fs ~/dotfiles/screen-256color.ti ~
+fi
