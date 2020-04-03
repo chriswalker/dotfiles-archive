ade88c6235b6d51f7e9e609750bb6ef0c52802bc
Chris Walker <thatamazingweb@gmail.com>
2020-04-03 14:01:36 +0100

    Set default colours for fzf and update /usr/local/Cellar/go@1.13/1.13.8/libexec

--- fish/config.fish
+++ fish/config.fish
@@ -28,9 +28,14 @@ set -x FZF_DEFAULT_COMMAND 'rg --hidden -l ""'
 
 # Set Nord theming
 set -x FZF_DEFAULT_OPTS '
-    --color fg:#D8DEE9,bg:#2E3440,hl:#88C0D0,fg+:#D8DEE9,hl+:#8F616A
-    --color pointer:#BF616A,info:#88C0D0,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B
+    --color fg:#D8DEE9,bg:#2E3440,hl:#88C0D0,fg+:#D8DEE9,bg+:#2E3440,hl+:#BF616A
+    --color pointer:#BF616A,info:#88C0D0,spinner:#88C0D0,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B
     --color gutter:#2E3440
+'
+
+# Set preview command
+set -x FZF_PREVIEW_COMMAND '
+    bat --theme="Nord" --color=always
 '
 
 # Show tree of selection in directory matching
@@ -49,7 +54,7 @@ if test -e ~/Dev/Go
   case Linux
     set -g GOROOT /usr/local/go
   case Darwin
-    set -g GOROOT /usr/local/Cellar/go/1.8/libexec
+    set -g GOROOT /usr/local/Cellar/go@1.13/1.13.8/libexec
   end
 
   # Update path
