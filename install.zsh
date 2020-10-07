9c77604f956d565648d4075be913fb3ebf7e7636
Chris Walker <chris.walker@karhoo.com>
2020-10-07 09:48:01 +0100

    Add fd ignore file

--- install.zsh
+++ install.zsh
@@ -1,21 +1,13 @@
 #!/bin/bash
 
+rm -rf ~/.config/fish ~/.config/nvim ~/.config/kitty ~/.config/kak
+
 ln -fs ~/dotfiles/tmux/.tmux.conf ~
 ln -fs ~/dotfiles/misc/.ripgreprc ~/.config/ripgrep
-#ln -fs ~/dotfiles/misc/kk ~/.kk
-
-rm -rf ~/.config/fish ~/.config/nvim ~/.config/kitty ~/.config/kak
+ln -fs ~/dotfiles/misc/fd ~/.config/fd
 
 ln -fs ~/dotfiles/fish ~/.config/fish
 ln -fs ~/dotfiles/nvim ~/.config/nvim
 ln -fs ~/dotfiles/kitty ~/.config/kitty
 ln -fs ~/dotfiles/kak ~/.config/kak
 ln -fs ~/dotfiles/alacritty ~/.config/alacritty
-
-
-# Tweaked terminfo for tmux's $TERM, replacing ^H (backspace)
-# with ASCII DEL. Allows Ctrl-h to work when remapped in (N)Vim/tmux
-# for moving to the next-left window
-#if [[ `uname` == 'Darwin' ]]; then
-#  ln -fs ~/dotfiles/screen-256color.ti ~
-#fi
