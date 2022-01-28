735dbf22e87a69a4a6b68df508c9ec356bbef781
Chris Walker <thatamazingweb@gmail.com>
2022-01-28 17:08:45 +0000

    Add wezterm config

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@
 rm -rf $XDG_CONFIG_HOME/fish
 
 DOTFILES_HOME=$HOME/dotfiles
-DOTS="bat fd fish git kak kitty nvim ripgrep"
+DOTS="bat fd fish git kak kitty nvim ripgrep wezterm"
 
-for DOT in $DOTS ; do
-    echo "Linking $DOT..."
-    ln -fs $DOTFILES_HOME/$DOT $XDG_CONFIG_HOME/
+for DOT in $DOTS; do
+  echo "Linking $DOT..."
+  ln -fs $DOTFILES_HOME/$DOT $XDG_CONFIG_HOME/
 done
